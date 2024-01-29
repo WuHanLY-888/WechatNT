ba126e95f574c5f6adf0a005d05c45194183fc77
李勇 <1834211049@qq.com>
2024-01-29 23:46:33 +0800

    fixed: 修复了，此前取消无法点击的问题，但是没有了动画，暂时先不管动画了

--- src/renderer/src/Pages/Login/QRCodeLogin.tsx
+++ src/renderer/src/Pages/Login/QRCodeLogin.tsx
@@ -1,24 +1,23 @@
-import { useEffect, useRef, useState } from 'react'
-import { Avatar, Button, Carousel, QRCode, QRCodeProps } from 'antd'
+import { useEffect, useState } from 'react'
+import { Avatar, Button, QRCode, QRCodeProps } from 'antd'
 import { ScanStatus } from 'wechaty-puppet/types'
 import { IpcRendererEvent } from 'electron/renderer'
 import styles from './login.module.less'
-import { CarouselRef } from 'antd/es/carousel'
 
 interface qrcodeStatusTypes {
     qrcode: string
     status: ScanStatus
     data?: string
 }
+
 const App: React.FC = () => {
     useEffect(() => {
         const { invoke, on: ipcOn } = window.ipc
 
         ipcOn('user-scan', (_: IpcRendererEvent, data: qrcodeStatusTypes) => {
-            console.log({ data });
-            console.log(ScanStatus[data.status]);
             setCodeVal(data.qrcode)
             setStatus('active')
+            setLoginStatus(data.status)
             switch (data.status) {
                 case 0: // 未知
 
@@ -31,7 +30,6 @@ const App: React.FC = () => {
                     break;
                 case 3: // 被扫了
                     setUserAvatar(data.data)
-                    carRef.current?.next()
                     break;
                 case 4: // 完成了
 
@@ -44,8 +42,7 @@ const App: React.FC = () => {
             }
         })
         invoke('get-scan').then((data: qrcodeStatusTypes) => {
-            console.log(data);
-
+            setLoginStatus(data.status)
             if (!data.qrcode) return
             setCodeVal(data.qrcode)
             setStatus('active')
@@ -59,13 +56,18 @@ const App: React.FC = () => {
         setTimeout(setStatus, 1000, 'active')
     }
 
-    const carRef = useRef<CarouselRef>(null)
     const [userAvatarUrl, setUserAvatar] = useState<string>()
+    const [loginStatus, setLoginStatus] = useState<ScanStatus>(ScanStatus.Unknown)
+    const scanCancel = () => {
+        setLoginStatus(ScanStatus.Cancel)
+        setUserAvatar(void 0)
+    }
+
 
     return (
         <div>
-            <Carousel ref={carRef} dots={false}>
-                <div>
+            {(loginStatus <= 2 || loginStatus === 5) &&
+                <div className='ant-motion-fade'>
                     <div className={styles.top}>
                         <p style={{ marginTop: 4 }}>
                             扫码登陆微信
@@ -75,34 +77,36 @@ const App: React.FC = () => {
                         style={{ margin: '0 auto', padding: '0', borderRadius: '0' }}
                         errorLevel="H"
                         size={150}
-                        color='#424242'
+                        color='#424242' // #fcfcfd
                         bordered={false}
                         status={status}
                         onRefresh={reFreshQRcode}
                         value={codeVal}
                     />
 
-                </div>
-                <div>
+                </div>}
+            {(loginStatus >= 3 && loginStatus <= 4) &&
+                <div className='ant-motion-fade'>
                     <div className={styles.top}
                         style={{ height: 400 }}>
                         <Avatar shape="square" size={80}
                             src={userAvatarUrl} />
                         <p style={{ marginTop: 30 }}>
-                            需要在手机上进行确定
+                            {
+                                loginStatus === 3
+                                    ? '需要在手机上进行确定' : '正在登陆'
+                            }
                         </p>
-                        <div className={styles.cancelBtn}>
-                            <Button type="link"
-                                style={{
-                                    color: '#5b6b92',
-                                }}>
-                                取消
-                            </Button>
-
-                        </div>
+                        <Button type="link"
+                            className={styles.cancelBtn}
+                            onClick={scanCancel}
+                            style={{
+                                color: '#5b6b92',
+                            }}>
+                            取消
+                        </Button>
                     </div>
-                </div>
-            </Carousel>
+                </div>}
         </div>
     )
 }
